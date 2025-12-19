35ab04c52fb6e286d75027cda98f74079b3357f7
ayendisimeon1 <mrayendi1@gmail.com>
2025-12-19 10:52:38 +0100

    pr update

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { getAuthToken } from './auth';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/v1',
+  baseURL: 'https://kiqi-server-pqqr.onrender.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
