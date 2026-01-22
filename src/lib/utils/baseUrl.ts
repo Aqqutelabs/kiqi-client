57d8a468e76714fbfb61abc1480d41b6a19e2566
ayendisimeon1 <mrayendi1@gmail.com>
2026-01-22 12:59:43 +0100

    update

--- src/lib/utils/baseUrl.ts
+++ src/lib/utils/baseUrl.ts
@@ -1,7 +1,7 @@
 
-// const BASE_URL = 'https://kiqi-server-pqqr.onrender.com';
+const BASE_URL = 'https://kiqi-server-pqqr.onrender.com';
 
 
-const BASE_URL = 'http://localhost:8000';
+// const BASE_URL = 'http://localhost:8000';
 
 export default BASE_URL;
