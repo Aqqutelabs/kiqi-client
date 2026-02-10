234b3462313b06f0f54428df96440d9da545765c
Aqqutelabs <aqqute.dev@gmail.com>
2026-02-10 12:05:23 +0100

    Remove commented BASE_URL assignment

--- src/lib/utils/baseUrl.ts
+++ src/lib/utils/baseUrl.ts
@@ -1,5 +1,5 @@
-
-const BASE_URL = 'https://kiqi-server.onrender.com';
+const BASE_URL = 'https://kiqi-server-1.onrender.com';
+// const BASE_URL = 'https://kiqi-server.onrender.com';
 
 
 // const BASE_URL = 'http://localhost:8000';
